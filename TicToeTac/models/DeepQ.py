a74a9d937e7a48d13c8835bb3fe45efc9b000304
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2023-06-01 18:00:34 -0400

    Made some changes in the environment but also, built a simple DQN Agent

--- TicToeTac/models/DeepQ.py
+++ TicToeTac/models/DeepQ.py
@@ -1,14 +1,28 @@
 import numpy as np
 from collections import deque
+from NN import Actor
 
 
 class DeepQ:
-    def __init__(self, epsilon_decay, learning_rate, env, memory_size):
+    def __init__(
+        self,
+        state_size,
+        action_size,
+        epsilon_decay,
+        learning_rate,
+        discount_factor,
+        epsilon_min,
+        memory_size,
+    ):
         self.epsilon_decay = epsilon_decay
         self.learning_rate = learning_rate
-        self.env = env
+        self.discount_factor = discount_factor
+        self.epsilon_min = epsilon_min
         self.epsilon = 1.0
         self.memory = deque(memory_size)
+        self.state_size = state_size
+        self.action_size = action_size
+        self.model = Actor(self.state_size, self.action_size)
 
     def generate_random_coords(self):
         coords = []
@@ -22,19 +36,33 @@ class DeepQ:
 
         return coords
 
+    # Transition is in form of tuple containing: state, action, reward, next_state, done
+    def remember(self, transition):
+        self.memory.append((transition))
+
     def takeAction(self, state):
-        pass
+        if np.random.rand() <= self.epsilon:
+            return np.random.randint(self.action_size)
 
-    def train(self, episodes=350):
-        for ep in range(episodes):
-            total_rewards = 0
-            done = False
-            current_state = self.env.reset()
-            while not done:
-                action = self.takeAction(state=current_state)
-                next_state, done, reward, info = self.env.step(action)
-                total_rewards += reward
+        action_value = self.model.predict(state)
+        return np.argmax(action_value[0])
 
-    def eval(self, episodes=10):
-        for ep in episodes:
-            pass
+    def replay(self, batch_size):
+        minibatch = np.random.choice(len(self.memory), batch_size, replace=False)
+        for transition in minibatch:
+            target = transition[2]
+            if not transition[4]:
+                target = transition[2] + self.discount_factor * np.amax(
+                    self.model.predict(transition[3])[0]
+                )
+            target_f = self.model.predict(transition[0])
+            target_f[0][transition[1]] = target
+            self.model.fit(transition[0], target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load_model(self, filename):
+        self.model.load_weights(filename)
+
+    def save_model(self, filename):
+        self.model.save_weights(filename)
